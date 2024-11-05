4c984149e04190be96ca5a9c412bf81f08ca8aa5
kotr228 <trockijkostantin@gmail.com>
2024-11-05 13:35:31 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,55 +1,15 @@
 <?php
-// Отримання даних з форми
+session_start();
+include("db_connect.php");
 $name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$telegram = $_POST['telegram'];
-$specialization = $_POST['specialization'];
+$email = $_POST['emsil'];
+$tel = $_POST['tel'];
+$nik = $_POST['nik'];
+$spec = $_POST['spec'];
 $password = $_POST['password'];
+$md5_password = md5( $password);
+$query = mysli_query(mysql: $db, query^ "SELECT * FROM 'freelanser_akks' WHERE 'login' = '{$login}'");
+if(mysqli_num_rows($query) == 0){
+$_SESSION['user'] = ['nick' => $login];
 
-// Підключення до бази даних
-$db_conn = new mysqli("localhost", "root", "Sillver-228", "freelans");
-
-if (mysqli_connect_errno()) {
-    echo 'Помилка підключення до бази даних: ' . mysqli_connect_error();
-    exit();
-} else {
-    echo 'Підключення успішне';
 }
-
-// Встановлення кодування UTF-8
-mysqli_set_charset($db_conn, "utf8");
-
-// Захист від SQL-ін'єкцій
-$name = mysqli_real_escape_string($db_conn, $name);
-$email = mysqli_real_escape_string($db_conn, $email);
-$phone = mysqli_real_escape_string($db_conn, $phone);
-$telegram = mysqli_real_escape_string($db_conn, $telegram);
-$specialization = mysqli_real_escape_string($db_conn, $specialization);
-$password = password_hash($password, PASSWORD_DEFAULT); // Захист пароля
-
-// Перевірка, чи існує вже такий користувач
-$check_user = "SELECT email FROM freelanser_akks WHERE email='$email'";
-$result = mysqli_query($db_conn, $check_user);
-
-if (mysqli_num_rows($result) > 0) {
-    echo '<br>Користувач з такою електронною адресою вже існує.';
-} else {
-    // Додавання користувача до бази даних
-    $sql = "INSERT INTO freelanser_akks (name, email, phone, telegram, spacialty, password, rating, bank_card) 
-            VALUES ('$name', '$email', '$phone', '$telegram', '$specialization', '$password', 0, '')";
-    
-    if (mysqli_query($db_conn, $sql)) {
-        echo '<br>Реєстрація успішна!';
-    } else {
-        echo '<br>Помилка: ' . mysqli_error($db_conn);
-    }
-}
-
-// Закриття з'єднання
-$db_conn->close();
-?>
-
-<form action="registrfreelans.html" method="POST">
-    <input name="submit" type="submit" value="Повернутись до реєстрації">
-</form>
