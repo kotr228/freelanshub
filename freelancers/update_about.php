3d01a5b74faa0d69d691fe47ea256a26e1f22363
kotr228 <trockijkostantin@gmail.com>
2024-12-08 06:58:54 +0200

    Create update_about.php

---
+++ freelancers/update_about.php
@@ -0,0 +1,24 @@
+<?php
+include('db_connect.php');
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $new_about = $_POST['new_about'];
+    $user_id = $_SESSION['user_id'];
+
+    // Оновлення в базі даних
+    $query = "UPDATE freelans.freelanser_akks SET about = ? WHERE id_f = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("si", $new_about, $user_id);
+    
+    if ($stmt->execute()) {
+        header('Location: correcting-info.php?success=1');
+    } else {
+        header('Location: correcting-info.php?error=1');
+    }
+    exit();
+}
+?>
