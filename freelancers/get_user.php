ed066a3b100dd3c9323ca7e2b6981bd111401751
kotr228 <trockijkostantin@gmail.com>
2024-12-08 06:58:49 +0200

    Update get_user.php

--- freelancers/get_user.php
+++ freelancers/get_user.php
@@ -19,10 +19,24 @@ if ($user_id) {
     $stmt->bind_param("i", $user_id);
     $stmt->execute();
     $stmt->bind_result($name, $avatar);
+    
     if ($stmt->fetch()) {
         $user_name = $name;
         $avatar_path = $avatar ? "uploads/avatars/$avatar" : $avatar_path;
+       
     }
+    $stmt->close();
+    // Припускаємо, що $user_id вже доступний
+    $query = "SELECT about FROM freelans.freelanser_akks WHERE id_f = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    $user_about = $row['about'] ?? 'Інформація не заповнена.';
+
+
     $stmt->close();
 }
 ?>
