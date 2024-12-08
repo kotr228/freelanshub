303eb6696183536e7ebae94a2aa643dacaf0d8f0
kotr228 <trockijkostantin@gmail.com>
2024-12-08 11:57:38 +0200

    Create update_em.php

---
+++ clients/update_em.php
@@ -0,0 +1,37 @@
+<?php
+include('db_connect.php');
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    die("Користувач не авторизований.");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $new_em = trim($_POST['new_em']);
+    $user_id = $_SESSION['user_id'];
+
+    if (empty($new_em)) {
+        die("Телеграм не може бути порожнім.");
+    }
+
+    $query = "UPDATE freelans.cliants_akks SET email = ? WHERE id_c = ?";
+    $stmt = $conn->prepare($query);
+
+    if (!$stmt) {
+        die("Помилка запиту: " . $conn->error);
+    }
+
+    $stmt->bind_param("si", $new_em, $user_id);
+
+    if ($stmt->execute()) {
+        header("Location: correcting-info.php?success=1");
+        exit();
+    } else {
+        echo "Помилка оновлення імені: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
