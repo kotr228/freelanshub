3c5bbc8e0c931c2716f166e392ba51a77a914cfe
kotr228 <trockijkostantin@gmail.com>
2025-01-30 03:25:22 +0200

    Update pupliczacaz.php

--- clients/pupliczacaz.php
+++ clients/pupliczacaz.php
@@ -10,51 +10,12 @@ include('get_user.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Делать заказы</title>
   <link rel="stylesheet" href="style.css/pupliczacaz.css">
-  
 </head>
 <body class="body">
 <div class="site">
   <header class="header">
-  <div class="header_item"><a href="#modal" class="btn-open-modal">Цінова політика</a>
-
-<!-- Модальне вікно -->
-<div id="modal">
-  <div class="modal-content">
-    <h3>Цінова політика</h3>
-     <h6>Платформа забирає певну частину платежу на утримання хостингу сайту. Для різних типів замовлень різні відсотки, також відсоток залежить від ціни.</h6>
-     <br><h4>Залежність відсотку від ціни замовлення:</h4>
-     <h6>До 1000 грн/25 usd - 5%</h6>
-     <h6>Від 1000 грн/25 usd до 42000 грн/1000 usd - 10%</h6>
-     <h6>Від 42000 грн/1000 usd до 84000 грн/2000 usd - 12%</h6>
-     <h6>Від 84000 грн/2000 usd - 15%</h6>
-     <br><h4>УВАГА!</h4>
-     <h6>Платформа робить платіж автоматично після сплати замовлення клієнтом, а клієнт може сплатити гроші за замовлення тільки після того, як виконавець повідомить про виконану роботу</h6><br>
-    <div class="modal-actions">
-     <a href="#" class="btn-close">Закрити</a>
-   </div>
-  </div>
-</div>
-</div>
-
-<div class="header_item"><a href="#modalph" class="btn-open-modal">Політика конфеденційності</a>
-    
-    <div id="modalph">
-      <div class="modal-contentph">
-        <h3>Політика конфеденційності</h3>
-         <h6>Платформа не збирає ваші персональні данні окрім пошти, номеру телефону та інших засобів зв'язку, також нам необхідна банківська картка виконавця. Також модератори не можуть змінити ваші паролі, оскільки у нас немає до них доступу, пізніше з'явиться можливість зміни паролю користувачем.</h6>
-         <br><h4>Файли:</h4>
-         <h6>Ми зберігаємо файли, які ви прикріплюєте до обмінників файлів, але ми не використовуємо їх. Також ми можемо надати ваші файли при необхідності, якщо ви втратите доступ до них.</h6>
-         <h6>Якщо є необхідність передати не файл, а проект, необхідно архівувати його.</h6>
-         <br><h4>УВАГА!</h4>
-         <h6>Платформа з часом видаляє старі файли, тобто вони можуть зберігатися рік, як і всі замовлення, тобто через рік після створення замовлення, система видаляє замовлення з бази даних, а також всі файли, які пов'язані з цим замовленням.</h6><br>
-        <div class="modalph-actions">
-         <a href="#" class="btn-closeph">Закрити</a>
-       </div>
-      </div>
-    </div>
-
-  </div>
-
+    <div class="header_item">Цінова політика</div>
+    <div class="header_item">Політика конфедеційності</div>
     <div class="header_item">Служба підтримки</div>
     <button class="header_item" onclick="location.href='peregladat_zakazy.php'">На головну</button>
     <div class="header_user-info">
@@ -63,20 +24,10 @@ include('get_user.php');
       <img class="logo-user" src="<?php echo htmlspecialchars($avatar_path); ?>" alt="">
       </a>
       <div class="dropdown-menu">
-    <button class="dropdown-item" onclick="location.href='loginclients.html'">Змінити аккаунт</button>
-    <button class="dropdown-item" onclick="location.href='registerclients.html'">Вийти з аккаунту</button>
-    <button class="dropdown-item" onclick="location.href='correcting-info.php'">Налаштування аккаунту</button>
-    <div class="dropdown-item">  <a href="#modaln" class="btn-open-modaln">Сповіщення</a>
-  <!-- Модальне вікно для сповіщень -->
-  <div id="notificationModal" class="modal">
-  <div class="modal-content">
-  <button class="btn-open-modal-notifications">Сповіщення</button>
-    <ul id="notificationList"></ul>
-    <button class="btn-close" onclick="closeModal()">Закрити</button>
-  </div>
-</div>
-  </div> 
-  </div>   
+        <button class="dropdown-item">Змінити аккаунт</button>
+        <button class="dropdown-item">Вийти з аккаунту</button>
+        <button class="dropdown-item">Налаштування аккаунту</button>
+      </div>
     </div>
   </header>
   
