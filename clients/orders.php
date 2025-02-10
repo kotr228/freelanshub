f1c8d6f509240e6460000b8adf99e690bf93ac42
kotr228 <trockijkostantin@gmail.com>
2025-02-10 11:54:44 +0200

    Update orders.php

--- clients/orders.php
+++ clients/orders.php
@@ -68,5 +68,22 @@ if ($result->num_rows === 0) {
     die("Немає замовлень для user_id = $user_id та фільтра $filter.");
 }
 
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<div class='block_info'>";
+        echo "<p>Назва: " . htmlspecialchars($row['lable']) . "</p>";
+        echo "<p>Спеціальність: " . htmlspecialchars($row['spacsalyty']) . "</p>";
+        echo "<p>Тип: " . htmlspecialchars($row['tipe']) . "</p>";
+        echo "<p>Статус: " . htmlspecialchars($row['status']) . "</p>";
+        echo "<div class='data_price'>";
+        echo "<p>Срок до: " . htmlspecialchars($row['date']) . "</p>";
+        echo "<p>Ціна: " . htmlspecialchars($row['price']) . "</p>";
+        echo "<a href='infojobfree.php?id_j=" . htmlspecialchars($row['id_j']) . "' class='header_item'>Детальніше</a>"; // Кнопка
+        echo "</div>";
+        echo "</div>";
+    }
+} else {
+    echo "<p>Немає замовлень за заданими параметрами.</p>";
+}
 
 ?>
