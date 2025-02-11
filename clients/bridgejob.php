65483f77952fb29fe6bee6022d566d63bda669b2
kotr228 <trockijkostantin@gmail.com>
2025-02-11 12:33:34 +0200

    Update bridgejob.php

--- clients/bridgejob.php
+++ clients/bridgejob.php
@@ -9,13 +9,13 @@ if (!isset($_SESSION['user_id'])) {
     die("Ви повинні бути авторизовані.");
 }
 
-$kay = (int)$_GET['kay'];
-$id_j = (int)$_GET['id_j'];
+$kay = isset($_GET['kay']) ? intval($_GET['kay']) : 0;
+$id_j = isset($_GET['id_j']) ? intval($_GET['id_j']) : 0;
 
 if (!is_numeric($kay) || !is_numeric($id_j)) {
     die("Неправильні параметри.");
 }
-header("Location: infojobclients.php?id_j = id_j");
+header("Location: infojobclients.php?id_j = . $id_j");
 if ($kay == 1) {
     header("Location: infojobclients.php?id_j = . $id_j");
 } elseif($kay == 2) {
