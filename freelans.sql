c1a4d8f546d79a5db55040c5cb0c0bdbae40d9ac
Shogun <182403974+Shogun-Ei@users.noreply.github.com>
2025-02-15 20:47:04 +0200

    Update freelans.sql

--- freelans.sql
+++ freelans.sql
@@ -206,6 +206,16 @@ CREATE TABLE IF NOT EXISTS `chat_files` (
     ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS 'notifications' (
+    'id' INT AUTO_INCREMENT PRIMARY KEY,
+    'user_id' INT NOT NULL,
+    'message' TEXT NOT NULL,
+    'is_read' TINYINT(1) DEFAULT 0,
+    'created_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
 
 -- Таблиця для статусу повідомлень
 CREATE TABLE IF NOT EXISTS `freelans`.`message_status` (
